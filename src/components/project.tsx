718788f5b5b9a56410aa5371ad4c6b899dae27dd
karancoder <jivanikaran@gmail.com>
2023-09-24 19:30:08 +0530

    fix: Projects responsive modes

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -23,8 +23,8 @@ export function Project({ title, description, tags, imageUrl }: ProjectProps) {
       ref={ref}
       className="group mb-3 last:mb-0 sm:mb-8"
     >
-      <section className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 group-even:pl-8 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[20rem] sm:pr-8">
-        <div className="flex h-full flex-col px-5 pb-7 pt-4 group-even:ml-[18rem] sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10">
+      <section className="relative max-w-[42rem] overflow-hidden rounded-lg border border-black/5 bg-gray-100 transition hover:bg-gray-200 dark:bg-white/10 dark:text-white dark:hover:bg-white/20 sm:h-[20rem] sm:pr-8 sm:group-even:pl-8">
+        <div className="flex h-full flex-col px-5 pb-7 pt-4 sm:max-w-[50%] sm:pl-10 sm:pr-2 sm:pt-10 sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <ul className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
@@ -44,19 +44,18 @@ export function Project({ title, description, tags, imageUrl }: ProjectProps) {
           src={imageUrl}
           alt={title}
           quality={95}
-          className="absolute -right-40 top-8 min-h-[90%] w-[28.25rem] rounded-t-lg shadow-2xl 
-        transition 
+          className="absolute -right-40 top-8 hidden min-h-[90%] w-[28.25rem] rounded-t-lg shadow-2xl transition 
         group-even:-left-40 
         group-even:right-[initial] 
-
         group-hover:-translate-x-3 
+
         group-hover:translate-y-3 
         group-hover:-rotate-2 
         group-hover:scale-[1.04] 
-
         group-even:group-hover:translate-x-3 
+
         group-even:group-hover:translate-y-3 
-        group-even:group-hover:rotate-2"
+        group-even:group-hover:rotate-2 sm:block"
         />
       </section>
     </motion.div>
