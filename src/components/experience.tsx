ff7b8bbdee71e332bac555fcee5e79d60bf5adb3
karancoder <jivanikaran@gmail.com>
2023-09-24 19:20:45 +0530

    fix:

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -32,15 +32,22 @@ function Experience({}: ExperienceProps) {
                 padding: "1.3rem 2rem",
               }}
               contentArrowStyle={{
-                borderRight: "0.4rem solid #9ca3af",
+                borderRight:
+                  theme === "light"
+                    ? "0.4rem solid #9ca3af"
+                    : "0.4rem solid rgba(255, 255, 255, 0.5)",
               }}
               date={experience.date}
               icon={experience.icon}
-              iconStyle={{ background: "white", fontSize: "1.5rem" }}
+              iconStyle={{
+                background:
+                  theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+                fontSize: "1.5rem",
+              }}
             >
               <h3 className="font-semibold capitalize">{experience.title}</h3>
               <p className="!mt-0 font-normal">{experience.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700">
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {experience.description}
               </p>
             </VerticalTimelineElement>
